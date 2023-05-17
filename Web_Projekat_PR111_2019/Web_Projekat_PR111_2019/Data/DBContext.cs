c684ff675a96d1b748acb1078b2ae099ec1bb2fa
jovanalazetic00 <101815250+jovanalazetic00@users.noreply.github.com>
2023-05-17 15:55:29 +0200

    Create initial project and connection to the DataBase

---
+++ Web_Projekat_PR111_2019/Web_Projekat_PR111_2019/Data/DBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web_Projekat_PR111_2019.Data
+{
+    public class DBContext:DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+
+        }
+    }
+}
